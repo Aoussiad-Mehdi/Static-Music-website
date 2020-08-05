68d851e9d28ec428de2f550a5c3f0b26c546fed7
Mehdi Aoussiad <mehdiouss315@gmail.com>
2020-08-05 20:47:51 +0100

    fixing some bugs

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // toggle buttoin functionality.
 const navbar = document.querySelector('.navbar');
 const icon = document.querySelector('#icon');
+const newNavbar = document.querySelector('.active');
 //adding a class to the navbar.
 function addClass(){
     navbar.classList.toggle('active');
-    navbar.style.top='80px';
     //Changing the icon by following the conditions above.
     if(navbar.classList != 'navbar'){
         icon.className = 'fas fa-times';
