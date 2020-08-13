0f39c21221cf16ba5359f42179f049eae693c26c
Mehdi Aoussiad <mehdiouss315@gmail.com>
2020-08-13 23:31:38 +0100

    contact section

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-// toggle buttoin functionality.
+// constatnts.
 const navbar = document.querySelector('.navbar');
 const icon = document.querySelector('#icon');
+const email = document.querySelector('#email');
+const number = document.querySelector('#number');
+const emailBtn = document.querySelector('#showemail');
+const numberBtn = document.querySelector('#phone');
+const text = [
+    'aoussiad92@gmail.com',
+    '********************'
+];
+const num = [
+    '0623232245',
+    '**********'
+];
+// We use this to loop over text and num arrays. 
+var counter = 0;
+
 //adding a class to the navbar.
 function addClass(){
     navbar.classList.toggle('active');
@@ -18,3 +33,32 @@ function navigation(){
     navbar.classList.remove('active');
     icon.className = 'fas fa-bars';
 }
+// contact section functionality.
+function contact(){
+    email.textContent = text[counter];
+    counter++;
+    if(counter === 1){
+        emailBtn.textContent = 'Hide Email';
+    }else{
+        emailBtn.textContent = 'Show Email';
+
+    }
+    if(counter === 2){
+        counter = 0;
+    }
+}
+function phone(){
+    number.textContent = num[counter];
+    counter++;
+    if(counter === 1){
+        numberBtn.textContent = 'Hide Number';
+    }else{
+        numberBtn.textContent = 'Show Number';
+
+    }
+    if(counter === 2){
+        counter = 0;
+    }
+}
+
+
